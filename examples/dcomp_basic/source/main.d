96061aeb67792d7c2a9057cd1a5670fcf7fff46c
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2017-04-26 20:56:20 +0900

    added example content

--- examples/dcomp_basic/source/main.d
+++ examples/dcomp_basic/source/main.d
@@ -1,9 +1,9 @@
 
 import core.sys.windows.windows;
 import std.windows.charset;
-import std.utf;
 
 import directx.d3d11, directx.d2d1_1, directx.dcomp;
+import directx.d2d1helper : D2D1;
 
 // missing ex window flags
 immutable WS_EX_NOREDIRECTIONBITMAP = 0x00200000;
@@ -17,27 +17,56 @@ private void checkFailure(HRESULT hr)
 class GraphicsDevice
 {
     ID3D11Device device3; ID2D1Device device2; IDCompositionDesktopDevice compDevice;
-    IDCompositionTarget target; IDCompositionVisual2 root;
+    IDCompositionTarget target; IDCompositionVisual2 root; IDCompositionVisual2[3] rects; IDCompositionSurface surface; IDCompositionScaleTransform scaler;
 
     this(HWND hwTarget)
     {
+        // Initialize Direct3D, Direct2D and DirectComposition
         IDXGIDevice devicex;
         D3D11CreateDevice(null, D3D_DRIVER_TYPE_HARDWARE, null,
             D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_DEBUG, null, 0, D3D11_SDK_VERSION,
             &this.device3, null, null).checkFailure();
         this.device3.QueryInterface(&IID_IDXGIDevice, cast(void**)&devicex).checkFailure();
         scope(exit) devicex.Release();
-        const cp = D2D1_CREATION_PROPERTIES(D2D1_THREADING_MODE_SINGLE_THREADED, D2D1_DEBUG_LEVEL_ERROR, D2D1_DEVICE_CONTEXT_OPTIONS_NONE);
+        immutable cp = D2D1_CREATION_PROPERTIES(D2D1_THREADING_MODE_SINGLE_THREADED, D2D1_DEBUG_LEVEL_ERROR, D2D1_DEVICE_CONTEXT_OPTIONS_NONE);
         D2D1CreateDevice(devicex, &cp, &this.device2).checkFailure();
         DCompositionCreateDevice2(this.device2, &IID_IDCompositionDesktopDevice, cast(void**)&this.compDevice).checkFailure();
 
+        // Create Composition Target and Visuals
         this.compDevice.CreateTargetForHwnd(hwTarget, FALSE, &this.target).checkFailure();
         this.compDevice.CreateVisual(&this.root).checkFailure();
+        foreach(ref e; this.rects) this.compDevice.CreateVisual(&e).checkFailure();
         this.target.SetRoot(this.root).checkFailure();
+        this.root.AddVisual(this.rects[0], true, null).checkFailure();
+        this.root.AddVisual(this.rects[1], true, null).checkFailure();
+        this.root.AddVisual(this.rects[2], true, null).checkFailure();
+
+        // Create Content(Surface) and Scaling Transform
+        this.compDevice.CreateSurface(1, 1, DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ALPHA_MODE_PREMULTIPLIED, &this.surface).checkFailure();
+        {
+            ID2D1DeviceContext con; POINT offs;
+            this.surface.BeginDraw(null, &IID_ID2D1DeviceContext, cast(void**)&con, &offs).checkFailure();
+            scope(exit) { this.surface.EndDraw().checkFailure(); con.Release(); }
+            immutable Transform = D2D1.Matrix3x2F.Translation(offs.x, offs.y);
+            immutable ClearColor = D2D1_COLOR_F(0.125f, 0.125f, 0.125f, 0.375f);
+
+            con.SetTransform(cast(D2D_MATRIX_3X2_F*)&Transform); con.Clear(&ClearColor);
+        }
+        this.compDevice.CreateScaleTransform(&this.scaler).checkFailure();
+        this.scaler.SetScaleX(100).checkFailure(); this.scaler.SetScaleY(100).checkFailure();
+        foreach(i, e; this.rects)
+        {
+            e.SetContent(this.surface).checkFailure();
+            e.SetTransform(this.scaler).checkFailure();
+            e.SetOffsetX(16 + i * 16).checkFailure(); e.SetOffsetY(16 + i * 16).checkFailure();
+        }
+
         this.compDevice.Commit().checkFailure();
     }
     ~this()
     {
+        this.scaler.Release(); this.surface.Release();
+        foreach(ref e; this.rects) e.Release();
         this.root.Release(); this.target.Release();
         this.compDevice.Release(); this.device2.Release(); this.device3.Release();
     }
@@ -60,7 +89,7 @@ extern(Windows) int WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
         const wndclass = RegisterClassExA(&wce);
         if(wndclass == 0) throw new Exception("RegisterClassEx failed");
 
-        auto hw = CreateWindowExW(WS_EX_NOREDIRECTIONBITMAP | WS_EX_APPWINDOW, cast(wchar*)wndclass, "dcomp example".toUTF16z,
+        auto hw = CreateWindowExA(WS_EX_NOREDIRECTIONBITMAP | WS_EX_APPWINDOW, cast(char*)wndclass, "dcomp example".toMBSz,
             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, null, null, hInstance, null);
         if(hw is null) throw new Exception("CreateWindowEx failed");
 
@@ -86,192 +115,3 @@ extern(Windows) nothrow LRESULT WndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
     default: return DefWindowProc(hw, msg, wp, lp);
     }
 }
-
-/+
-import core.sys.windows.windows;
-import core.stdc.stdio;
-import core.stdc.string;
-import std.stdio;
-import std.string;
-import std.utf;
-
-import directx.d3d11;
-
-// our objects instances, keep in mind that this is thread-local
-ID3D11Device device;
-ID3D11DeviceContext context;
-IDXGISwapChain swapchain;
-ID3D11RenderTargetView backbuffer;
-
-/////////////////////////
-// Windows specific stuff
-
-extern(Windows)
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    int result;
-
-	import core.runtime;
-	try
-    {
-        Runtime.initialize();
-		result = myWinMain();
-		Runtime.terminate();
-    }
-
-    catch (Exception e)            // catch any uncaught exceptions
-    {
-        MessageBox(null, toUTF16z(e.msg), "Error", MB_OK | MB_ICONEXCLAMATION);
-        result = 0; // failed
-    }
-
-	return result;
-}
-
-
-extern(Windows)
-LRESULT WindowProc(HWND hWnd, uint uMsg, WPARAM wParam, LPARAM lParam) nothrow
-{
-    switch (uMsg)
-    {
-        case WM_COMMAND:
-            break;
-			
-        case WM_PAINT:
-            break;
-
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-
-        default:
-            break;
-    }
-
-    return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
-
-int myWinMain()
-{
-	HINSTANCE hInst = GetModuleHandle(null);
-    WNDCLASS  wc;
-
-    wc.lpszClassName = "DWndClass";
-    wc.style         = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc   = &WindowProc;
-    wc.hInstance     = hInst;
-    wc.hIcon         = LoadIcon(cast(HINSTANCE) null, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(cast(HINSTANCE) null, IDC_CROSS);
-    wc.hbrBackground = cast(HBRUSH) (COLOR_WINDOW + 1);
-    wc.lpszMenuName  = null;
-    wc.cbClsExtra    = wc.cbWndExtra = 0;
-
-    auto a = RegisterClass(&wc);
-    assert(a); //note that asserts only run in debug mode
-
-    HWND hWnd;
-    hWnd = CreateWindow("DWndClass", "D3D Window", WS_THICKFRAME |
-                         WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_SYSMENU | WS_VISIBLE,
-                         CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, HWND_DESKTOP,
-                         cast(HMENU) null, hInst, null);
-    assert(hWnd);
-
-  
-    // init our pipeline
-    InitD3D(hWnd);
-    
-    // our main loop
-    MSG msg;
-    while(true)
-    {
-        if( PeekMessage(&msg, null, 0, 0, PM_REMOVE) ){
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-            
-            if( msg.message == WM_QUIT )
-                break;
-        }
-
-        // clear, draw, present
-        RenderFrame();
-    }
-    
-    // release resources
-    CleanD3D();
-
-    return S_OK;
-}
-
-///////////////////////////////////////
-//// Direct3D stuff
-
-void InitD3D(HWND hWnd)
-{
-    // create a struct to hold information about the swap chain
-    DXGI_SWAP_CHAIN_DESC scd;
-
-    // fill the swap chain description struct
-    scd.BufferCount = 1;                                    // one back buffer
-    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
-    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
-    scd.OutputWindow = hWnd;                                // the window to be used
-    scd.SampleDesc.Count = 4;                               // how many multisamples
-    scd.Windowed = TRUE;                                    // windowed/full-screen mode
-
-    // create a device, device context and swap chain using the information in the scd struct
-    D3D11CreateDeviceAndSwapChain(null,
-                                  D3D_DRIVER_TYPE_HARDWARE,
-                                  null,
-                                  0,
-                                  null,
-                                  0,
-                                  D3D11_SDK_VERSION,
-                                  &scd,
-                                  &swapchain,
-                                  &device,
-                                  null,
-                                  &context);
-
-    // get the address of the back buffer
-    ID3D11Texture2D pBackBuffer;
-    swapchain.GetBuffer(0, &IID_ID3D11Texture2D, cast(void**)&pBackBuffer);
-    
-    // use the back buffer address to create the render target
-    device.CreateRenderTargetView(pBackBuffer, null, &backbuffer);
-    pBackBuffer.Release();
-
-    // set the render target as the back buffer
-    context.OMSetRenderTargets(1, &backbuffer, null);
-    
-    // Set the viewport
-    D3D11_VIEWPORT viewport;
-    viewport.TopLeftX = 0;
-    viewport.TopLeftY = 0;
-    viewport.Width = 800;
-    viewport.Height = 600;
-
-    context.RSSetViewports(1, &viewport);
-}
-
-void RenderFrame()
-{
-    float[4] color = [0.0f, 0.2f, 0.4f, 1.0f];
-    // clear the back buffer to a deep blue
-    context.ClearRenderTargetView(backbuffer, color.ptr);
-
-    // do 3D rendering on the back buffer here
-
-    // switch the back buffer and the front buffer
-    swapchain.Present(0, 0);
-}
-
-void CleanD3D()
-{
-    // close and release all existing COM objects
-    if(swapchain) swapchain.Release();
-    if(backbuffer) backbuffer.Release();
-    if(device) device.Release();
-    if(context) context.Release();
-}
-+/
